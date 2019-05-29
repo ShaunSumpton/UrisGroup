cf4172d4e4be291a09ad0b2cfb6c47dc7b12df26
Shaun Sumpton <s.sumpton@agnortheast.com>
2019-05-29 10:51:24 +0100

    Add project files.

---
+++ UrisGroup/BBS.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UrisGroup;
+using SortAndSave;
+
+
+namespace UrisGroup
+{
+    public class BBS
+    {
+
+
+
+        public static void BBSNow(String en, String md, String jn)
+        {
+            DateTime Newmd = DateTime.Parse(md);
+            string dir = Path.GetDirectoryName(en);
+            Directory.CreateDirectory(dir + "\\" + "BBS");
+            
+            BBSAPI.SetInstallationFolder("C:\\SORTANDSAVE");
+            BBSAPI.SetOutputBase(dir + "\\" + "BBS");
+            BBSAPI.SetWeight(30);
+            BBSAPI.SetDescription(jn);
+            BBSAPI.SetReference(jn);
+            BBSAPI.SetCollectionDate(md.ToString());
+            BBSAPI.SetHandoverDate(Newmd.AddDays(1).ToString());
+
+
+
+            var MyIni = new IniFile("C:\\TEST FOLDER\\URIS.JOB");
+            MyIni.Write("Weight", "100", "InitialInfo");
+
+
+
+
+
+            // Check for Default Auto Net/One Call Job
+
+
+
+            //result = BBSAPI.RunMailingJob();
+
+
+
+
+
+
+
+        }
+
+
+
+    }
+
+}
---
+++ UrisGroup/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PgpCore;
+
+namespace UrisGroup
+{
+    public partial class UrisGroup : Form
+    {
+        public UrisGroup()
+        {
+            InitializeComponent();
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            string EncryptedFiles;
+            string Password = "1dunn0d0u";
+            string key = @"C:\TEST FOLDER\secring.skr";
+            string JobNumber = textBox1.Text;
+            string MailDate = textBox2.Text;
+
+
+            if (textBox2.Text.Length == 0)
+            {
+
+                MessageBox.Show("Please Enter the Mailing Date"); // check if a mailing date has been entered
+                this.Close();
+                Application.Exit();
+                System.Environment.Exit(0);
+            }
+
+            if (textBox2.Text.Length == 0)
+            {
+
+                MessageBox.Show("Please Enter the Job Number"); // check if a job number has been entered
+                this.Close();
+                Application.Exit();
+                System.Environment.Exit(0);
+            }
+
+
+            OpenFileDialog openFileDialog2 = new OpenFileDialog(); // open file dialog to select encrypted file
+            openFileDialog2.Filter = "pgp files (*.pgp)|*.pgp|All files (*.*)|*.*";
+            openFileDialog2.ShowDialog();
+
+            EncryptedFiles = openFileDialog2.FileName;
+            MessageBox.Show(EncryptedFiles);
+
+            DecryptFiles(EncryptedFiles, Password, key, JobNumber);
+
+            BBS.BBSNow(EncryptedFiles, MailDate, JobNumber);
+
+
+            //CreateData();
+
+            //Composer():
+
+        }
+
+        public void DecryptFiles(string EncryptedFiles, string Password, string key, string JobNumber)
+        {
+            using (PGP pgp = new PGP())
+            {
+                string directory = Path.GetDirectoryName(EncryptedFiles);
+
+                pgp.DecryptFile(@EncryptedFiles, Path.Combine(directory, JobNumber + ".xls"), @key, Password);
+
+
+            }
+
+
+        }
+
+        
+    }
+}
